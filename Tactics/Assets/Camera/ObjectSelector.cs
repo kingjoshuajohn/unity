cbfbaa1e211127f3ed49a6b1ac4521e2e87afe9a
kingjoshuajohn <kingjoshuajohn@gmail.com>
2016-07-04 17:04:58 -0500

    Initial Commit
    Initial Commit

---
+++ Tactics/Assets/Camera/ObjectSelector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSelector : MonoBehaviour {
+
+    public LayerMask layerMask;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit && Utilities.isInLayerMask(hitInfo.transform.gameObject,layerMask))
+            {
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+            }
+        }
+    }
+}
---
+++ Tactics/Assets/Scripts/Utility/Utilities.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utilities
+{
+
+    public static bool isInLayerMask(GameObject obj, LayerMask mask)
+    {
+        return ((mask.value & (1 << obj.layer)) > 0);
+    }
+}
